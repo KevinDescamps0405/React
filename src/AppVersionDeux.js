f3552751bc021754c7bfc7c286c34e1aaef7fcf8
Kevin Descamps <kevin.descamps2@soprasteria.com>
2022-05-25 16:06:00 +0200

    Test

--- src/AppVersionDeux.js
+++ src/AppVersionDeux.js
@@ -31,9 +31,10 @@ const testData = [
 ];
 
 const CardList = (props) => (
-    <div>
-        {testData.map(profile => <Card {...profile}/>)}
-    </div>
+        <div>
+            {props.profiles.map(profile => <Card {...profile}/>)}
+        </div>
+
 );
 
 class Card extends React.Component {
@@ -51,12 +52,33 @@ class Card extends React.Component {
     }
 }
 
+class Form extends React.Component {
+    render() {
+        return (
+            <form action="">
+                <input type="text" placeholder="Github username"/>
+                <button>Add Card</button>
+            </form>
+        );
+    }
+}
+
 class AppVersionDeux extends React.Component {
+/*    constructor(props) {
+        super(props);
+        this.state = {
+            profiles:testData,
+        };
+    }*/
+    state = {
+        profiles : testData,
+    }
     render() {
         return (
             <div>
                 <div className="header">{this.props.title}</div>
-                <CardList/>
+                <Form/>
+                <CardList profiles={this.state.profiles}/>
             </div>
         );
     }
